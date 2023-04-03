8b872f0d365ba33d8954c5df2cbba7f131534b84
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-04-03 18:56:51 -0400

    load extension locally

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 /* Major Features */
 
-// 1. Display map markers for local crime data
+// 1. Display map markers for local crime data, each providing users with crime type, date, location, and CSI (crime severity index) score
+
+// 2. Display map markers for local police stations, each providing users with station name, address, and phone number

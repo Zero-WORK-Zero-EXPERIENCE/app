ee3d087306b80c7762efdfebd3663d5003bc1113
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-04-13 01:15:21 -0400

    Add onInstalled func

--- js/background.js
+++ js/background.js
@@ -1,5 +1,13 @@
 // IIFE ensures service worker runs continuously in the background
 (() => {
+  // Sets initial state or completes tasks when extension is installed
+  chrome.runtime.onInstalled.addListener(() => {
+    // Using extension badge on browser toolbar to indicate whether extension is on or off
+    chrome.action.setBadgeText({
+      text: 'OFF',
+    });
+  });
+
   // Checks if user's current (or most recently opened) tab is on Google Maps
   chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes('google.com/maps')) {
